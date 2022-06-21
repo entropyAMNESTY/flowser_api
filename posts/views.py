c340e0d3480b30c2a24236d57984a444e0531a11
entropyAMNESTY <entropy.random.squared@gmail.com>
2022-06-21 11:50:52 +0200

    Functionality: delting votes and posts by user with restriction to only delete their items

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.forms import ValidationError
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, mixins, status
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from .models import Post, Vote
 from .serializers import PostSerializer, VoteSerializer
 
@@ -13,7 +14,21 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)
 
-class VoteCreate(generics.CreateAPIView):
+#Delete Posts Functionality
+class PostRetrieveDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+#Limit users permission to delete the posts, only they created
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], poster=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('This isn\'t your post to delete, BRUH!')
+
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -27,3 +42,13 @@ class VoteCreate(generics.CreateAPIView):
         if self.get_queryset().exists():
             raise ValidationError('You have already voted for this post :)')
         serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        # Fallback when a user tries to delete an upvote which isn't existent
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            # Response if a vote exists
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            # Response when a vote doesn't exist
+            raise ValidationError('You never voted for this post... silly!')
