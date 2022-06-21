5e5b5df9ba23a13a6295ef84d50721cb29a182ce
entropyAMNESTY <entropy.random.squared@gmail.com>
2022-06-21 11:01:48 +0200

    Functionality: Check if user already voted + Login-Link for users integrated

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
+from django.forms import ValidationError
 from django.shortcuts import render
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
 from .models import Post, Vote
 from .serializers import PostSerializer, VoteSerializer
 
@@ -21,4 +23,7 @@ class VoteCreate(generics.CreateAPIView):
         return Vote.objects.filter(voter=user, post=post)
 
     def perform_create(self, serializer):
+        # Checking if the user already had a vote on the same post
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post :)')
         serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
